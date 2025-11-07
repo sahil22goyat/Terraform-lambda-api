d57a77197ef94de4c2458d7ad79e58668ef20a29
Sahil Goyat <131314392+sahil22goyat@users.noreply.github.com>
2025-11-07 17:55:40 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -104,11 +104,4 @@ resource "aws_lambda_permission" "apigw" {
   source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
 }
 
-output "api_endpoint" {
-  value = aws_apigatewayv2_api.http_api.api_endpoint
-}
-
-output "lambda_name" {
-  value = aws_lambda_function.demo.function_name
-}
 
